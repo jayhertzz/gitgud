e4ed333b0afc3af62c1f5f4daa796b2653640ab7
mscragg08 <112993190+mscragg08@users.noreply.github.com>
2022-09-22 21:47:17 -0400

    methodized in-line code
    made in-line code into methods. integrated more document types

--- gitgud_003.py
+++ gitgud_003.py
@@ -111,7 +111,8 @@ from string import punctuation
 import textract
 from docx2pdf import convert  # for converting docx to pdf, for page counting
 import re
-import docx  # python-docx
+
+
 
 
 # condition text: lower-case, remove punctuation, remove some misc. items
@@ -138,7 +139,6 @@ def extract_text(filepath, extn):
 
 # pulls ALL matching files from a given directory and its subdirectories
 def walk_directory(root_path):
-    compatible_types = ['doc', 'docx', 'pdf', 'epub', 'html', 'pptx', 'rtf', 'txt']
     file_name_list = []
     for root, dirs, files in os.walk(root_path, topdown=False):
         for name in files:
@@ -186,24 +186,157 @@ def convert_docx_to_pdf(docx_file_path):
     return convert(docx_file_path)
 
 
+def perform_search_nopage(text, keywords):
+    file_keyword_search_results = []
+    for keyword in keywords:  # do the following subroutine for each keyword in the list 'keywords'
+        keyword = keyword.lower()
+        text_body = preprocess_text_keep_punc(text)
+        keyword_count = text_body.count(keyword)
+        prev_CR_count = 0  # initialize the carriage return counter var
+        prev_word_count = 0  # initialize the word count var
+        prev_sentence_count = 0  # initialize the word count var
+
+        for j in range(keyword_count):
+            char_find_index = text_body.find(keyword)  # search a subset of the original text body
+            if char_find_index > -1:  # only make a record if a match is found (char_find_index == -1 means no match)
+                """ count CRs """
+                trunc_text = text_body[:char_find_index]
+                # count the number of carriage returns to keep track of line num.
+                cr_count = count_CRs(trunc_text) + prev_CR_count
+                prev_CR_count = cr_count  # keep track of previous CR counts to optimize next run CR counting operation
+
+                """ count words """
+                word_count = prev_word_count + count_words(trunc_text)
+                prev_word_count = word_count
+
+                """ count sentences """
+                sentence_count = prev_sentence_count + count_sentences(trunc_text)
+                prev_sentence_count = sentence_count
+
+                """ store results """
+                result = (keyword, char_find_index, cr_count, word_count, sentence_count)
+                file_keyword_search_results.append(result)
+
+                """ setup the next run """
+                text_body = text_body[char_find_index + 1:]  # truncate the text body to exclude this run's found keyword
+    return file_keyword_search_results
+
+
+def perform_search_with_page(page_text, keywords):
+    file_keyword_search_results = []
+    for i in range(len(page_text)):  # do the following subroutine for each page
+        page_num = i  # intuitive var name to hold page number for result storage
+
+        for keyword in keywords:  # do the following subroutine for each keyword in the list 'keywords'
+            text_body = preprocess_text_keep_punc(page_text[i])
+            keyword = keyword.lower()
+            page_keyword_count = text_body.count(keyword)
+            prev_CR_count = 0  # initialize the carriage return counter var
+            prev_word_count = 0  # initialize the word count var
+            prev_sentence_count = 0  # initialize the word count var
+
+            for j in range(page_keyword_count):
+                char_find_index = text_body.find(keyword)  # search a subset of the original text body
+                if char_find_index > -1:  # only make a record if a match is found (char_find_index == -1 means no match)
+                    """ count CRs """
+                    trunc = text_body[:char_find_index]
+                    # count the number of carriage returns to keep track of line num.
+                    cr_count = count_CRs(trunc) + prev_CR_count
+                    prev_CR_count = cr_count  # keep track of previous CR counts to optimize next run CR counting operation
+
+                    """ count words """
+                    word_count = prev_word_count + count_words(trunc)
+                    prev_word_count = word_count
+
+                    """ count sentences """
+                    sentence_count = prev_sentence_count + count_sentences(trunc)
+                    prev_sentence_count = sentence_count
+
+                    """ store results """
+                    result = (keyword, page_num, char_find_index, cr_count, word_count, sentence_count)
+                    file_keyword_search_results.append(result)
+
+                    """ setup the next run """
+                    text_body = text_body[char_find_index + 1:]  # truncate the text body to exclude this run's found keyword
+    return file_keyword_search_results
+
+
+def count_paragraphs(docx_file):
+    return len(docx_file.paragraphs)
+
+
+def get_text(file_path, file_extn, page_req_flag=False):
+    if page_req_flag:
+        # switch matrix for file extension handling
+        if file_extn == 'pdf':
+            pdfFileObj = open(pdf_list[1], 'rb')  # create a file handler for reading in a pdf file object
+            pdfReader = PyPDF2.PdfFileReader(pdfFileObj)  # Call/initialize the PyPDF2 library
+            num_pages = len(pdfReader.pages)  # read # of pages from the pdf doc
+            page_text = []
+            for page in range(int(num_pages)):  # use int to make sure it's a whole number value
+                page_text.append(
+                    pdfReader.getPage(page).extractText())  # append each page's text to a new index in page_text list
+            return page_text
+
+        elif file_extn == 'docx':
+            convert_docx_to_pdf(file_path)
+            new_file_path = file_path.replace('.docx', '.pdf')
+            return get_text(new_file_path, 'pdf', True)
+
+        # elif file_extn == 'epub':
+        #     epub_text = epub.read_epub(file_path)
+        #     print('here')
+
+
+    else:
+        # TODO: build a section here to decode the text based on input file format e.g. .decode('UTF-8')
+        return extract_text(file_path, file_extn)
+
+
+def process_epub_for_pages(epub_text):
+    epub_pages = []
+    page_index = 0
+    while (page_index > -1):
+        page_index
+
+
 """
+USER INPUTS
+&
 TEST FLAGS
 """
 # enable your debugging sections using these flags
-doc_flag = docx_flag = pdf_flag = epub_flag = html_flag = pptx_flag = rtf_flag = txt_flag = False
-pdf_flag = True
-doc_flag = True
-docx_flag = True
+doc_flag = docx_flag = pdf_flag = epub_flag = txt_flag = \
+           pptx_flag = rtf_flag = html_flag = docx_conv_flag = False
 
+# pdf_flag = True
+# # doc_flag = True
+# docx_flag = True
+epub_flag = True
+
+keywords = ['and']  # small set used for testing. guaranteed hits
+
+# context user inputs for nearby words
+context_keywords = ['the']  # small set used for testing. guaranteed hits
+min_sentence_filter = 0
+max_sentence_filter = 5
+min_word_filter = 0
+max_word_filter = 10
 """
 END TEST FLAGS
 """
 
-fpath_1 = r'D:\applications\PyCharm Projects\gitgud_01\test_documents_01'
-page_text = ''  # initialize variable to hold text for testing
-# text_files = []
+# change this file path (fpath) to your test document repo directory. Be sure to retain the 'r' in front
+test_fpath = r'D:\applications\PyCharm Projects\gitgud_01\test_documents_01'
 
-file_list = walk_directory(fpath_1)
+# initialize variable to hold text for testing
+page_text = ''
+
+# list of document file types we can parse:
+compatible_types = ['html', 'txt', 'doc', 'docx', 'pdf', 'epub', 'pptx', 'rtf']
+
+# find all files in a directory:
+file_list = walk_directory(test_fpath)
 
 """
 RTF TESTING SECTION
@@ -212,9 +345,26 @@ RTF TESTING SECTION
 test_extn_rtf = 'rtf'
 rtf_list = find_file_ext(file_list, test_extn_rtf)
 if rtf_list and rtf_flag:  # check for empty lists first
-    test_text_rtf = preprocess_text_remove_punc(str(extract_text(rtf_list[0], test_extn_rtf)))
-    print(test_text_rtf)
-    test_text = test_text_rtf
+    rtf_file_path = rtf_list[0]
+    # test_text_rtf = str(extract_text(rtf_list[0], test_extn_rtf))
+    # print(test_text_rtf)
+    # test_text = test_text_rtf
+    rtf_text = get_text(rtf_file_path, test_extn_rtf)
+    rtf_nopage_results = perform_search_nopage(rtf_text, keywords)
+
+"""
+.TXT TESTING SECTION
+"""
+
+test_extn_txt = 'txt'
+txt_list = find_file_ext(file_list, test_extn_txt)
+if txt_list and txt_flag:  # check for empty lists first
+    txt_file_path = txt_list[0]
+    # test_text_txt = str(extract_text(txt_list[0], test_extn_txt))
+    # print(test_text_txt)
+    # test_text = test_text_txt
+    txt_text = get_text(txt_file_path, test_extn_txt)
+    txt_nopage_results = perform_search_nopage(txt_text, keywords)
 
 """
 PDF TESTING SECTION
@@ -224,7 +374,8 @@ test_extn_pdf = 'pdf'
 pdf_list = find_file_ext(file_list, test_extn_pdf)
 if pdf_list and pdf_flag:  # check for empty lists first
     """
-    FIRST TEST HERE IS WITH TEXTRACT LIBRARY
+    FIRST TEST HERE IS WITH TEXTRACT LIBRARY, which destroys page #s
+    --deprecated
     """
     # test_text_pdf = ((extract_text(pdf_list[1], test_extn_pdf)).decode('UTF-8'))
     # # test_text_pdf = preprocess_text((extract_text(pdf_list[1], test_extn_pdf)).decode('UTF-8'))
@@ -232,39 +383,38 @@ if pdf_list and pdf_flag:  # check for empty lists first
     # test_text = test_text_pdf
 
     """
-    SECOND TEST HERE IS WITH PYPDF2 LIBRARY
+    SECOND TEST HERE IS WITH PYPDF2 LIBRARY, which retains page #s
     """
-    pdfFileObj = open(pdf_list[1], 'rb')  # create a file handler for reading in a pdf file object
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)  # Call/initialize the PyPDF2 library
-    num_pages = len(pdfReader.pages)  # read # of pages from the pdf doc
-    page_text = []
 
-    for page in range(int(num_pages)):  # use int to make sure it's a whole number value
-        page_text.append(
-            pdfReader.getPage(page).extractText())  # append each page's text to a new index in page_text list
+    pdf_text = get_text(pdf_list[1], test_extn_pdf, True)
+    pdf_results = perform_search_with_page(pdf_text, keywords)
 
 """
-DOC/DOCX TESTING SECTION
+DOCX TESTING SECTION
 """
-
+pdf_creation_list = []  # keep track of created pdfs for destruction later
 test_extn_docx = 'docx'
 docx_list = find_file_ext(file_list, test_extn_docx)
 if docx_list and docx_flag:  # check for empty lists first
+    docx_file_path = docx_list[1]
     # test_text_docx = preprocess_text((extract_text(docx_test_file, test_extn_docx)).decode('UTF-8'))
 
-    """slow docx-to-pdf conversion but successful"""
-    # for docx_file in docx_list:
-    #     convert_docx_to_pdf(docx_file)
-
     """python-docx code"""
-    test_doc = docx.Document(docx_list[1])
-    print('The number of paragraphs in this document is: {}'.format(len(test_doc.paragraphs)))
+    # test_docx = docx.Document(docx_list[1])
+    # print('The number of paragraphs in this document is: {}'.format(count_paragraphs(test_docx)))
+
+    pdf_creation_list.append(docx_file_path)
+    docx_paged_text = get_text(docx_file_path, test_extn_docx, True)  # this will create a pdf of the docx file first
+    docx_paged_results = perform_search_with_page(docx_paged_text, keywords)
     """end python-docx code"""
 
     """textract code"""
-    test_text_docx = ((extract_text(docx_list[0], test_extn_docx)).decode('UTF-8'))
-    print(test_text_docx)
-    test_text = test_text_docx
+    # test_text_docx = ((extract_text(docx_file_path, test_extn_docx)).decode('UTF-8'))
+    # print(test_text_docx)
+    # test_text = test_text_docx
+    docx_nopage_text = get_text(docx_file_path, test_extn_docx)
+    docx_nopage_results = perform_search_nopage(docx_nopage_text, keywords)
+
     """end textract code"""
 
 """
@@ -274,9 +424,36 @@ HTML TESTING SECTION
 test_extn_html = 'html'
 html_list = find_file_ext(file_list, test_extn_html)
 if html_list and html_flag:  # check for empty lists first
-    test_text_html = ((extract_text(html_list[0], test_extn_html)).decode('UTF-8'))
-    print(test_text_html)
-    test_text = test_text_html
+    html_file = html_list[0]
+    html_text = get_text(html_file, test_extn_html)
+
+"""
+PPTX TESTING SECTION
+"""
+
+test_extn_pptx = 'pptx'
+pptx_list = find_file_ext(file_list, test_extn_pptx)
+if pptx_list and pptx_flag:  # check for empty lists first
+    pptx_file = pptx_list[0]
+    pptx_nopage_text = get_text(pptx_file, test_extn_pptx)
+
+"""
+EPUB TESTING SECTION
+"""
+
+test_extn_epub = 'epub'
+epub_list = find_file_ext(file_list, test_extn_epub)
+if epub_list and epub_flag:  # check for empty lists first
+    epub_file = epub_list[0]
+
+    """ebooklib code"""
+    # epub_paged_text = get_text(epub_file, test_extn_epub, True)
+    """end ebooklib code"""
+
+    """textract code"""
+    epub_nopage_text = (get_text(epub_file, test_extn_epub)).decode('UTF-8')
+    epub_nopage_results = perform_search_nopage(epub_nopage_text, keywords)
+    """end textract code"""
 
 """
 MISC DEBUG SECTION
@@ -287,56 +464,11 @@ MISC DEBUG SECTION
 
 """
 FIND THE LOCATIONS OF KEYWORDS IN A GIVEN BODY OF TEXT
-keyword matches on the sanitized data set will not yield much in the way of location data.
-    It will yield page #, but not word #, due to false-positive word parsing of punctuation and other factors.
-    
-We will eventually need to feed back some parameters of the search result to the front-end and elsewhere
-
-DEPENDENCIES:
-    the functionality of this algorithm depends on the data structure which:
-    1 text page = 1 list index (i.e. page_text is a list(type))
 """
-keywords = ['adopt']
 
-# TODO: Make this search algorithm into a callable definition/method. Possibly a recursive implementation
-file_keyword_search_results = []
-files_results = {}
+# see each document section
 
-# this section only works for pdfs at the moment
-if pdf_flag:
-    for i in range(len(page_text)):  # do the following subroutine for each page
-        page_num = i  # intuitive var name to hold page number for result storage
 
-        for keyword in keywords:  # do the following subroutine for each keyword in the list 'keywords'
-            text_body = page_text[i].lower()
-            page_keyword_count = text_body.count(keyword.lower())
-            prev_CR_count = 0  # initialize the carriage return counter var
-            prev_word_count = 0  # initialize the word count var
-            prev_sentence_count = 0  # initialize the word count var
-
-            for ja in range(page_keyword_count):
-                new_index = text_body.find(keyword.lower())  # search a subset of the original text body
-                if new_index > -1:  # only make a record if a match is found (new_index == -1 means no match)
-                    """ count CRs """
-                    trunc = text_body[:new_index]
-                    # count the number of carriage returns to keep track of line num.
-                    cr_count = count_CRs(trunc)
-
-                    """ count words """
-                    word_count = prev_word_count + count_words(trunc)
-                    prev_word_count = word_count
-
-                    """ count sentences """
-                    sentence_count = prev_word_count + count_sentences(trunc)
-                    prev_word_count = sentence_count
-
-                    """ store results """
-                    result = (keyword, page_num, new_index, cr_count, word_count, sentence_count)
-                    file_keyword_search_results.append(result)
-
-                    """ setup the next run """
-                    prev_CR_count = cr_count  # keep track of previous CR counts to optimize next run CR counting operation
-                    text_body = text_body[new_index + 1:]  # truncate the text body to exclude this run's found keyword
 
 """
 TODO SECTION
